9a825128a18680984eaa2787e5323c01266f96ce
Smazhenko Vitaliy <Smazhenko.Vitaliy@gmail.com>
2023-07-20 10:41:19 +0300

    find all superheroes with pagination

--- server/controllers/superhero.controller.js
+++ server/controllers/superhero.controller.js
@@ -11,3 +11,15 @@ module.exports.createSuperhero = async(req,res,next) =>{
         next(error)
     }
 }
+
+module.exports.findAllSuperheroes = async(req, res, next) =>{
+    try{
+        const {pagination} = req;
+        const superheroes = await Superhero.findAll({
+            ...pagination
+        });
+        return res.status(200).send(superheroes)
+    } catch(err) {
+        next(err)
+    }
+}
--- server/routes/superhero.router.js
+++ server/routes/superhero.router.js
@@ -1,6 +1,7 @@
 const {Router} = require('express');
 const SuperheroController = require('../controllers/superhero.controller');
-const {validateSuperhero} = require('../middlewares/superhero.mw')
+const {validateSuperhero} = require('../middlewares/superhero.mw');
+const pagination = require('../middlewares/pagination.mw')
 
 
 
@@ -9,6 +10,7 @@ const {validateSuperhero} = require('../middlewares/superhero.mw')
 const superheroRouter = Router();
 
 superheroRouter.post('/', validateSuperhero, SuperheroController.createSuperhero);
+superheroRouter.get('/', pagination, SuperheroController.findAllSuperheroes)
 
 
 module.exports = superheroRouter;
